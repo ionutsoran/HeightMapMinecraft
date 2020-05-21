f699cc53c71147be063ff25692fa727f40503842
Ionut Soran <ionutsoran@yahoo.com>
2020-05-21 22:18:13 +0300

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="HeightmapMinecraft",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["PyQt5", "pip>=20.1.1"],
+)
